e99cf3a5d089e9175c950203893ad5c554976020
Jacob Krebs <89816523+JJosephK@users.noreply.github.com>
2021-09-09 21:30:58 -0500

    Add files via upload

---
+++ Week2AssessmentKrebs/src/test/AllTests.java
@@ -0,0 +1,16 @@
+package test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+/**
+ * @author krebs jjkrebs1
+ * CIS 175 - Fall 2021
+ * Sep 9, 2021
+ */
+@RunWith(Suite.class)
+@SuiteClasses({ CoolTest.class, DisposalTest.class })
+public class AllTests {
+
+}
